ea0b7da9648fa68ddb9e370eeba7d8032d25f1ae
EasanA <easan.arulanantham@gmail.com>
2017-01-04 00:26:15 +0000

    repr

--- bicycles.py
+++ bicycles.py
@@ -5,6 +5,9 @@ class Bike(object):
         self.cost_price = prod_cost
         self.selling_price = 0
 
+    def __repr__(self):
+        return ('model:{}: price:{}').format(self.name, self.selling_price)
+        
 class BikeShop(object):
     def __init__(self, bikes):
         self.margin = 1.2
@@ -13,10 +16,20 @@ class BikeShop(object):
             bike.selling_price = self.margin * bike.cost_price
     
 class Customer(object):
-    pass
+    def __init__(self, name, budget):
+        self.name = name
+        self.budget = budget
+        
+    def __repr__(self):
+        return ('name:{}: budget:{}').format(self.name, self.budget)
 
 if __name__ == '__main__':
     bike1 = Bike('bike1', 30, 100)
-    bike_models = [bike1]
+    bike2 = Bike('bike2', 20, 300)
+    bike_models = [bike1, bike2]
     bike_shop = BikeShop(bike_models)
     print(bike1.selling_price)
+    customer1 = Customer('bob', 200)
+    customers = [customer1]
+    print(bike_models)
+    print(customers)
