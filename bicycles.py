5926e5791c890f5afc6891d9b959173ce2b3c74c
EasanA <easan.arulanantham@gmail.com>
2017-01-09 02:06:31 +0000

    refactoring

--- bicycles.py
+++ bicycles.py
@@ -47,19 +47,3 @@ class Customer(object):
          self.budget -= bike.selling_price
          print(self.budget)
          bike_shop.update_inv(bike)
-        
-if __name__ == '__main__':
-    bike1 = Bike('bike1', 30, 100)
-    bike2 = Bike('bike2', 20, 300)
-    bike_models = {bike1: 4, bike2 : 2}
-    bike_shop = BikeShop(bike_models)
-    print(bike1.selling_price)
-    customer1 = Customer('bob', 200)
-    customers = [customer1]
-    print(bike_models)
-    print(bike_shop.bikes)
-    print(customers)
-    print(bike_shop.afford(customer1))
-    customer1.buy(bike_shop,bike1)
-    print(customer1)
-    print(bike_models)
--- main.py
+++ main.py
@@ -2,4 +2,17 @@ from bicycles import Bike, BikeShop, Customer
 
 
 if __name__ == '__main__':
-    pass
+    bike1 = Bike('bike1', 30, 100)
+    bike2 = Bike('bike2', 20, 300)
+    bike_models = {bike1: 4, bike2 : 2}
+    bike_shop = BikeShop(bike_models)
+    print(bike1.selling_price)
+    customer1 = Customer('bob', 200)
+    customers = [customer1]
+    print(bike_models)
+    print(bike_shop.bikes)
+    print(customers)
+    print(bike_shop.afford(customer1))
+    customer1.buy(bike_shop,bike1)
+    print(customer1)
+    print(bike_models)
