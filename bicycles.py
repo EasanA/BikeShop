175777664b804c3c92f03f200e4e2363a0efaa86
EasanA <easan.arulanantham@gmail.com>
2017-01-16 19:02:00 +0000

    extension: frame, wheels, and manufacturer

--- bicycles.py
+++ bicycles.py
@@ -1,12 +1,27 @@
 class Bike(object):
+    def __init__(self, name, wheel, frame, manufacturer):
+        self.name = name
+        self.weight = 2* wheel.weight + frame.weight
+        self.selling_price = 0
+        self.cost_price = 2* wheel.cost_price + frame.cost_price
+        self.manufacturer = manufacturer
+        self.prod_cost =  manufacturer.margin * self.cost_price
+
+    def __repr__(self):
+        return ('model:{}: price:{}').format(self.name, self.selling_price)
+
+        
+class Wheel(object):
     def __init__(self, name, weight, prod_cost):
         self.name = name
         self.weight = weight
         self.cost_price = prod_cost
-        self.selling_price = 0
 
-    def __repr__(self):
-        return ('model:{}: price:{}').format(self.name, self.selling_price)
+class Frame(object):
+    def __init__(self, name, weight, prod_cost):
+        self.name = name
+        self.weight = weight
+        self.cost_price = prod_cost
         
 class BikeShop(object):
     def __init__(self, bikes, profit, margin):
@@ -14,7 +29,7 @@ class BikeShop(object):
         self.bikes = bikes
         self.profit = profit
         for bike in bikes:
-            bike.selling_price = self.margin * bike.cost_price
+            bike.selling_price = self.margin * bike.prod_cost
             
             
     def afford(self, customer):
@@ -34,7 +49,11 @@ class BikeShop(object):
         self.profit += profit_per_bike 
         print("Bikeshop's profit:{}".format(self.profit))
         return(self.profit)
-
+    
+    def inv_check(self, bike):
+        #create a true/false value that is passed onto the buy
+        return self.bikes[bike] > 0
+            
 class Customer(object):
     def __init__(self, name, budget):
         self.name = name
@@ -44,10 +63,18 @@ class Customer(object):
         return ('name:{}: budget:{}').format(self.name, self.budget)
     
     def buy(self, bike_shop, bike):
-        if bike_shop.bikes[bike] <= 0:
-            print('Sorry, we are out of {}.'.format(bike))
-        else:    
+        if  bike_shop.inv_check(bike) and bike in bike_shop.afford(self):
             print('Bike sold:{}'.format(bike))
             self.budget -= bike.selling_price
             print("{}'s Leftover Budget:{}".format(self.name, self.budget))
             bike_shop.update_inv(bike)
+            return (True, 'Purchase successful')
+        elif bike not in bike_shop.afford(self):
+            return (False, 'Sorry,you cannot afford {}.'.format(bike))
+        else:
+            return (False, 'Sorry, we are out of {}.'.format(bike))
+            
+class BikeManufacturer(object):
+    def __init__(self,name, margin):
+        self.name = name
+        self. margin = margin
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from bicycles import Bike, BikeShop, Customer
+from bicycles import Bike, BikeShop, Customer, Wheel, Frame, BikeManufacturer
 
 
 if __name__ == '__main__':
-    bike1 = Bike('bike1', 30, 100)
-    bike2 = Bike('bike2', 20, 300)
-    bike3 = Bike('bike3', 22, 500)
-    bike4 = Bike('bike4', 19, 700)
-    bike5 = Bike('bike5', 15, 900)
-    bike6 = Bike('bike6', 25, 400)
-    bike_models = {bike1: 4, bike2 : 2, bike3 : 5, bike4 : 0, bike5 : 2, bike6: 4}
+    wheel1 = Wheel('wheel1', 5, 20)
+    wheel2 = Wheel('wheel2', 4, 100)
+    wheel3 = Wheel('wheel3', 3, 250)
+    frame1 = Frame('frame1', 10, 75)
+    frame2 = Frame('frame2', 8, 250)
+    frame3 = Frame('frame3', 4, 400)
+    manufacturer1 = BikeManufacturer('Manu1', 1.1)
+    manufacturer2 = BikeManufacturer('Manu2', 1.2)
+    bike1 = Bike('bike1', wheel1, frame1, manufacturer1)
+    bike2 = Bike('bike2', wheel1, frame2, manufacturer1)
+    bike3 = Bike('bike3', wheel2, frame2, manufacturer2)
+    bike4 = Bike('bike4', wheel3, frame2, manufacturer2)
+    bike5 = Bike('bike5', wheel2, frame3, manufacturer2)
+    bike6 = Bike('bike6', wheel3, frame3, manufacturer1)
+    bike_models = {bike1: 4, bike2 : 2, bike3 : 5, bike4 : 0, bike5 : 0, bike6: 4}
     bike_shop = BikeShop(bike_models, 0, 1.2)
     customer1 = Customer('Bob', 200)
     customer2 = Customer('Carol',500)
@@ -17,9 +25,9 @@ if __name__ == '__main__':
     print(bike_shop.bikes)
     print(customers)
     print('{} can afford {}'.format(customer1.name, bike_shop.afford(customer1)))
-    customer1.buy(bike_shop,bike1)
+    print(customer1.buy(bike_shop,bike1))
     print('{} can afford {}'.format(customer2.name, bike_shop.afford(customer2)))
-    customer2.buy(bike_shop,bike2)
+    print(customer2.buy(bike_shop,bike4))
     print('{} can afford {}'.format(customer3.name, bike_shop.afford(customer3)))
-    customer3.buy(bike_shop,bike4)
+    print(customer3.buy(bike_shop,bike5))
     print('Inventory:{}'.format(bike_models))
